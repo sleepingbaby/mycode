49c54af3a3bc88ac7b74b1fa9dd8d843a72a68c5
sleepingbaby <crosby2987@gmail.com>
2024-08-13 19:31:07 +0000

    Query Parameters - Level 3

--- query_params/lab97.py
+++ query_params/lab97.py
@@ -5,16 +5,57 @@ import requests
 import random
 import html
 
-URL= "https://opentdb.com/api.php?amount=10"
+URL= "https://opentdb.com/api.php"
 
 def main():
+    categories= {
+    9:  "General Knowledge", 
+    10: "Entertainment- Books", 
+    11: "Entertainment- Film", 
+    12: "Entertainment- Music", 
+    13: "Entertainment- Musicals & Theater", 
+    14: "Entertainment- Television", 
+    15: "Entertainment- Video Games", 
+    16: "Entertainment- Board Games", 
+    17: "Science- Nature", 
+    18: "Science- Computers", 
+    19: "Science- Mathematics", 
+    20: "Mythology", 
+    21: "Sports", 
+    22: "Geography", 
+    23: "History", 
+    24: "Politics", 
+    25: "Art", 
+    26: "Celebrities", 
+    27: "Animals", 
+    28: "Vehicles", 
+    29: "Entertainment- Comics", 
+    30: "Science- Gadgets", 
+    31: "Entertainment- - Japanese Anime & Manga", 
+    32: "Entertainment- - Cartoon Animations"
+    }
+
+    quantity = input("Enter the number of questions: ")
+    difficulty = input("Enter the difficulty (easy, medium, hard): ")
+
+    for category in categories:
+        print(f"{category}: {categories[category]}")
+    print('\n')
+    category = input("Enter the category ID (e.g, 9 for General Knowledge): ")
+
+    params = {
+        "amount": quantity,
+        "category": category,
+        "difficulty": difficulty
+    }
+
+    data = requests.get(URL, params).json()
 
     # data will be a python dictionary rendered from your API link's JSON!
-    data= requests.get(URL).json()
     questions = data["results"]
     random.shuffle(questions)
 
-    while True:
+    while questions:
         question = questions.pop()
         question_text = html.unescape(question["question"])
 
@@ -37,7 +78,7 @@ def main():
         else:
             print("Invalid input\n")
 
-
+    print("Thank you for playing!")
 
 if __name__ == "__main__":
     main()
